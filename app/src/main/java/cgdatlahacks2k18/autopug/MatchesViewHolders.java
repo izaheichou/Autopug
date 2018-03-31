eeac4986a771e0bacc5717f7c08096c99881cd68
Ryan Samia <ryansamia@me.com>
2018-03-31 16:27:59 -0700

    Changed from compile to implement and added matchesActivity

---
+++ app/src/main/java/cgdatlahacks2k18/autopug/MatchesViewHolders.java
@@ -0,0 +1,24 @@
+package cgdatlahacks2k18.autopug;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView mMatchID;
+    public MatchesViewHolders(View itemView)
+    {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchID = (TextView) itemView.findViewById(R.id.Matchid);
+    }
+
+    @Override
+    public void onClick(View view)
+        {
+
+        }
+
+}
